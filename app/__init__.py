b87b71d75dbebc2242d1ba9c9cd3b542854f905c
Peter Nielson <computergeekpete@gmail.com>
2019-02-19 20:52:13 -0700

    Added error handling for login errors; updated deploy Dockerfile for db

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_dance.contrib.google import make_google_blueprint, google
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -13,15 +13,13 @@ def create_app(object_name=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
     google_bp = make_google_blueprint(
-        client_id="102163899252-gu4pmkr3jq4ttjumhd2v69gj986n489c.apps.googleusercontent.com",
-        client_secret="FAGhh2s4tj_nQ-BtDRuGvFTf",
+        client_id=os.environ.get('GOOGLE_CLIENT_ID'),
+        client_secret=os.environ.get('GOOGLE_SECRET'),
         scope=[
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile",
             "https://www.googleapis.com/auth/plus.me"
-        ],
-        offline=True,
-        reprompt_consent=True
+        ]
     )
     app.register_blueprint(google_bp, url_prefix="/google-login")
     db.init_app(app)
